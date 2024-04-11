c160169c8037f6d43979b3b1e7086c52ff1897f2
rhenninger61 <rhenninger61@southhills.edu>
2024-04-11 15:29:36 -0400

    Pretty blinking lights

--- 01_Blink/Blink.py
+++ 01_Blink/Blink.py
@@ -1,26 +1,30 @@
 from gpiozero import LED as LEDClass # Alias
 import time
 
-LED = LEDClass(17)  # define led
-
+LED_17 = LEDClass(17)  # define led
+LED_18 = LEDClass(18)
 def loop():
-    global LED
+    global LED_17
+    global LED_18
     while True:
-        LED.on() 
-        print ("led turned on >>>") # print information on terminal
+        LED_17.on() 
+        LED_18.off()
+        print ("led 17 turned on, led 18 turned off >>>") # print information on terminal
         time.sleep(1)
-        LED.off()
-        print ("led turned off <<<")
+        LED_17.off()
+        LED_18.on()
+        print ("led 17 turned off, led 18 turned on <<<")
         time.sleep(1)
         
 def destroy():
-    global LED
+    global LED_17, LED_18
     # Release resources
-    LED.close()
+    LED_17.close()
+    LED_18.close()
 
 if __name__ == "__main__":    # Program start point
     print("Program is starting ... \n")
-    print(f"Using pin {LED.pin}")
+    print(f"Using pin {LED_17.pin} and {LED_18.pin}")
     try:
         loop()
     except KeyboardInterrupt:   # Press ctrl-c to end the program.
