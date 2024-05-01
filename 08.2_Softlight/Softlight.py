20dd67b326e6fe21b2210baf10584a355ea0480c
rhenninger61 <rhenninger61@southhills.edu>
2024-05-01 15:04:09 -0400

    2 working dimmers

--- 08.2_Softlight/Softlight.py
+++ 08.2_Softlight/Softlight.py
@@ -10,7 +10,8 @@ from ADCDevice import *
 
 USING_GRAVITECH_ADC = False # Only modify this if you are using a Gravitech ADC
 
-LED = PWMLED(17) #17 is the pin
+LED_17 = PWMLED(17) #17 is the pin
+LED_18 = PWMLED(18)
 ADC = ADCDevice() # Define an ADCDevice class object
 
 def setup():
@@ -28,20 +29,30 @@ def setup():
         exit(-1)
         
 def loop():
-    global LED
+    global LED_17, LED_18
     while True:
         # read the ADC value of channel 0
-        value = ADC.analogRead(0)  # Gets a value between 0 and 255
+        value_17 = ADC.analogRead(0)  # Gets a value between 0 and 255
         # Set LED brightness directly with value from ADC
-        LED.value = value / 255.0  # Value of PWM LED must be between 0 and 1
+        LED_17.value = value_17 / 255.0  # Value of PWM LED must be between 0 and 1
         # calculate the voltage value
-        voltage = value / 255.0 * 3.3  # 3.3 because we are using the 3.3V lead
-        print(f'ADC Value: {value} \tVoltage: {voltage:.2f}')
+        voltage = value_17 / 255.0 * 3.3  # 3.3 because we are using the 3.3V lead
+        print(f'ADC Value: {value_17} \tVoltage: {voltage:.2f}')
+        time.sleep(0.03)
+        
+        # read the ADC value of channel 1
+        value_18 = ADC.analogRead(1)  # Gets a value between 0 and 255
+        # Set LED brightness directly with value from ADC
+        LED_18.value = value_18 / 255.0  # Value of PWM LED must be between 0 and 1
+        # calculate the voltage value
+        voltage = value_18 / 255.0 * 3.3  # 3.3 because we are using the 3.3V lead
+        print(f'ADC Value: {value_18} \tVoltage: {voltage:.2f}')
         time.sleep(0.03)
 
 def destroy():
-    global LED, ADC
-    LED.close()
+    global LED_17, LED_18, ADC
+    LED_17.close()
+    LED_18.close()
     ADC.close()
     
 if __name__ == '__main__':   # Program entrance
